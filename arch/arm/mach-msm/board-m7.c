171a21e6927c6eb228dac30ef533a5f6e4f53e45
Paul Reioux <reioux@gmail.com>
2013-05-12 23:30:32 -0500

    EHCI: HSIC: Add support to vote for SVS digital voltage
    HSIC HW can operate at SVS digital voltage when it is active.
    Currently HSIC is voting for 120Mhz SFAB and 64Mhz DFAB clock
    frequencies. Clock frequencies are directly proportional to SVS
    voltage. If DFAB is running at 64Mhz or SFAB is running at 120Mhz
    or higher SVS is not possible even though HSIC votes for it. Hence
    do not vote for SFAB clock frequency and only vote for 32Mhz for
    DFAB in order to allow SVS to happen. Voting for SVS voltage reduces
    current consumption by 3mA in HSDPA network.
    
    Change-Id: I8a6844efbbe90a8ea118be8450f8210717c2641b
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    (cherry picked from commit cdf7fd2)
    (cherry picked from commit f7f3688)
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- arch/arm/mach-msm/board-m7.c
+++ arch/arm/mach-msm/board-m7.c
@@ -1493,12 +1493,6 @@ static struct i2c_board_info msm_i2c_mhl_sii9234_info[] =
 static struct msm_bus_vectors hsic_init_vectors[] = {
        {
                .src = MSM_BUS_MASTER_SPS,
-               .dst = MSM_BUS_SLAVE_EBI_CH0,
-               .ab = 0,
-               .ib = 0,
-       },
-       {
-               .src = MSM_BUS_MASTER_SPS,
                .dst = MSM_BUS_SLAVE_SPS,
                .ab = 0,
                .ib = 0,
@@ -1508,15 +1502,9 @@ static struct msm_bus_vectors hsic_init_vectors[] = {
 static struct msm_bus_vectors hsic_max_vectors[] = {
        {
                .src = MSM_BUS_MASTER_SPS,
-               .dst = MSM_BUS_SLAVE_EBI_CH0,
-               .ab = 60000000,		/* At least 480Mbps on bus. */
-	       .ib = 960000000,		/* MAX bursts rate */      
-       },
-       {
-               .src = MSM_BUS_MASTER_SPS,
                .dst = MSM_BUS_SLAVE_SPS,
                .ab = 0,
-               .ib = 512000000, /*vote for 64Mhz dfab clk rate*/
+               .ib = 256000000, /*vote for 32Mhz dfab clk rate*/
        },
 };
 
