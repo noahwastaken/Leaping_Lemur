f1d5112dea90227050b94ecefba43c5b60b15308
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2013-10-10 17:30:14 -0700

    ASoC: msm: qdsp6v2: Fix buffer overflow in voice driver
    Userspace registers calibration data with acdb driver
    through ioctls. Voice driver registers the calibration
    data with CVD by querying acdb data from acdb driver and
    copies the calibration data in apr message.
    
    The size of the calibration data can be controlled by userspace
    and can result in buffer overflow if the calibration size is
    greater than the destination buffer size.
    Reject acdb data if the size is greater than the size of
    destination buffer.
    
    Prev-Change-Id: I1b495390d67907afc608ed8233bf916777d38e11
    CRs-fixed: 548872
    Signed-off-by: Shiv Maliyappanahalli <smaliyap@codeaurora.org>
    
    Conflicts:
    
    	sound/soc/msm/qdsp6v2/q6voice.c
    
    Change-Id: Ic50d948134a72ab416be1a46ba59e33beb04827d

--- sound/soc/msm/qdsp6/q6voice.c
+++ sound/soc/msm/qdsp6/q6voice.c
@@ -1349,7 +1349,8 @@ static int voice_send_cvs_register_cal_cmd(struct voice_data *v)
 
 	
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVS_CAL_SIZE)
 		goto fail;
 
 	if (v == NULL) {
@@ -1421,7 +1422,8 @@ static int voice_send_cvs_deregister_cal_cmd(struct voice_data *v)
 	u16 cvs_handle;
 
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVS_CAL_SIZE)
 		return 0;
 
 	if (v == NULL) {
@@ -1478,7 +1480,8 @@ static int voice_send_cvp_map_memory_cmd(struct voice_data *v)
 
 	
 	get_all_cvp_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVP_CAL_SIZE)
 		goto fail;
 
 	if (v == NULL) {
@@ -1549,7 +1552,8 @@ static int voice_send_cvp_unmap_memory_cmd(struct voice_data *v)
 	uint32_t cal_paddr;
 
 	get_all_cvp_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVP_CAL_SIZE)
 		return 0;
 
 	if (v == NULL) {
@@ -1613,7 +1617,8 @@ static int voice_send_cvs_map_memory_cmd(struct voice_data *v)
 
 	
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVS_CAL_SIZE)
 		goto fail;
 
 	if (v == NULL) {
@@ -1685,7 +1690,8 @@ static int voice_send_cvs_unmap_memory_cmd(struct voice_data *v)
 	uint32_t cal_paddr;
 
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVS_CAL_SIZE)
 		return 0;
 
 	if (v == NULL) {
@@ -1749,7 +1755,8 @@ static int voice_send_cvp_register_cal_cmd(struct voice_data *v)
 
       
 	get_all_vocproc_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVP_CAL_SIZE)
 		goto fail;
 
 	if (v == NULL) {
@@ -1821,7 +1828,8 @@ static int voice_send_cvp_deregister_cal_cmd(struct voice_data *v)
 	u16 cvp_handle;
 
 	get_all_vocproc_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	if (cal_block.cal_size == 0 ||
+	    cal_block.cal_size > CVP_CAL_SIZE)
 		return 0;
 
 	if (v == NULL) {
@@ -1872,6 +1880,7 @@ static int voice_send_cvp_register_vol_cal_table_cmd(struct voice_data *v)
 	struct cvp_register_vol_cal_table_cmd cvp_reg_cal_tbl_cmd;
 	struct acdb_cal_block vol_block;
 	struct acdb_cal_block voc_block;
+	struct acdb_cal_block cvp_block;
 	int ret = 0;
 	void *apr_cvp;
 	u16 cvp_handle;
@@ -1880,8 +1889,10 @@ static int voice_send_cvp_register_vol_cal_table_cmd(struct voice_data *v)
 	
 	get_all_vocvol_cal(&vol_block);
 	get_all_vocproc_cal(&voc_block);
+	get_all_cvp_cal(&cvp_block);
 
-	if (vol_block.cal_size == 0)
+	if (vol_block.cal_size == 0 ||
+	    cvp_block.cal_size > CVP_CAL_SIZE)
 		goto fail;
 
 	if (v == NULL) {
@@ -1949,12 +1960,16 @@ static int voice_send_cvp_deregister_vol_cal_table_cmd(struct voice_data *v)
 {
 	struct cvp_deregister_vol_cal_table_cmd cvp_dereg_cal_tbl_cmd;
 	struct acdb_cal_block cal_block;
+	struct acdb_cal_block voc_block;
 	int ret = 0;
 	void *apr_cvp;
 	u16 cvp_handle;
 
 	get_all_vocvol_cal(&cal_block);
-	if (cal_block.cal_size == 0)
+	get_all_cvp_cal(&voc_block);
+
+	if (cal_block.cal_size == 0 ||
+	    voc_block.cal_size > CVP_CAL_SIZE)
 		return 0;
 
 	if (v == NULL) {
