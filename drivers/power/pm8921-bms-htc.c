f424e2eb55128124c4b3c5a9159ce30c514bf248
flar2 <asegaert@gmail.com>
2013-11-25 22:43:26 -0500

    Linaro GCC 4.8 fixes

--- drivers/power/pm8921-bms-htc.c
+++ drivers/power/pm8921-bms-htc.c
@@ -1650,7 +1650,7 @@ EXPORT_SYMBOL(pm8921_bms_get_vsense_avg);
 
 int pm8921_bms_get_battery_current(int *result_ua)
 {
-	int vsense;
+	int vsense = 0;
 
 	if (!the_chip) {
 		pr_err("called before initialization\n");
@@ -3038,7 +3038,7 @@ static int dump_cc_uah(void)
 int prev_cc_uah = 0;
 static int pm8921_bms_suspend(struct device *dev)
 {
-	u64 val;
+	u64 val = 0;
 #if 0 
 	int rc;
 	struct pm8xxx_adc_chan_result result;
@@ -3086,7 +3086,7 @@ static int pm8921_bms_suspend(struct device *dev)
 #define DELTA_RBATT_PERCENT	10
 static int pm8921_bms_resume(struct device *dev)
 {
-	u64 val;
+	u64 val = 0;
 #if 0 
 	struct pm8921_rbatt_params raw;
 	struct pm8921_bms_chip *chip = dev_get_drvdata(dev);
