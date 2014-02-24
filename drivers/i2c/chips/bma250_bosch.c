44e73667cde53ae092de1a985fdf24ba82809ec4
Lens-F <tnoah12@gmail.com>
2014-02-24 18:53:20 -0500

    fix power_key_check error

--- drivers/i2c/chips/bma250_bosch.c
+++ drivers/i2c/chips/bma250_bosch.c
@@ -1518,7 +1518,7 @@ extern void flick2wake_setdev(struct input_dev * input_device) {
 EXPORT_SYMBOL(flick2wake_setdev);
 
 static void flick2wake_presspwr(struct work_struct * flick2wake_presspwr_work) {
-	if ( touchscreen_is_on()==0 && power_key_check_in_pocket(0) ) return; // don't wake if in pocket
+	if ( touchscreen_is_on()==0 && power_key_check_in_pocket() ) return; // don't wake if in pocket
 
 	if (!mutex_trylock(&pwrlock))
 	    return;
@@ -1639,7 +1639,7 @@ static void pick2wake_count(struct work_struct * pick2wake_count_work) {
 	unsigned int calc_time = 0;
 	printk("BMA pick2wake_count - check ts on and pwp...\n");
 
-	if ( touchscreen_is_on()==0 && power_key_check_in_pocket(0) ) return; // don't wake if in pocket
+	if ( touchscreen_is_on()==0 && power_key_check_in_pocket() ) return; // don't wake if in pocket
 
 	printk("BMA pick2wake_count\n");
 	if (!mutex_trylock(&picklock))
