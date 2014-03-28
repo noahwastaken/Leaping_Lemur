27cbce0bc39fc7a5b0780a38f81a01347a4e2f03
Lens-F <tnoah12@gmail.com>
2014-03-28 19:14:00 -0400

    Sound Mods and track files

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -2950,6 +2950,8 @@ static int tabla_codec_enable_dec(struct snd_soc_dapm_widget *w,
 				  snd_soc_read(codec,
 				  tx_digital_gain_reg[w->shift + offset])
 				  );
+				snd_soc_write(codec, tx_digital_gain_reg[0], 10);
+				snd_soc_write(codec, tx_digital_gain_reg[1], 10);
 
 		break;
 
@@ -2994,6 +2996,9 @@ static int tabla_codec_reset_interpolator(struct snd_soc_dapm_widget *w,
 				  snd_soc_read(codec,
 				  rx_digital_gain_reg[w->shift])
 				  );
+				snd_soc_write(codec, rx_digital_gain_reg[0], 12);
+				snd_soc_write(codec, rx_digital_gain_reg[1], 12);
+
 		break;
 	}
 	return 0;
