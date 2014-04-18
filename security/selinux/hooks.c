73fc1e2a06d65784bf65cb61da96294654a658fc
Lens-F <tnoah12@gmail.com>
2014-04-18 13:35:16 -0400

    PATCH: Linux 3.4.78
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -193,6 +193,14 @@ static int inode_alloc_security(struct inode *inode)
 	return 0;
 }
 
+static void inode_free_rcu(struct rcu_head *head)
+{
+	struct inode_security_struct *isec;
+
+	isec = container_of(head, struct inode_security_struct, rcu);
+	kmem_cache_free(sel_inode_cache, isec);
+}
+
 static void inode_free_security(struct inode *inode)
 {
 	struct inode_security_struct *isec = inode->i_security;
