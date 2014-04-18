779853203e2879576b9f51cc929a51389639a2b6
Lens-F <tnoah12@gmail.com>
2014-04-18 13:08:14 -0400

    PATCH: Linux 3.4.59
    Signed-off-by: Lens-F <tnoah12@gmail.com>
    
    Conflicts:
    	kernel/futex.c

--- kernel/futex.c
+++ kernel/futex.c
@@ -61,6 +61,7 @@
 #include <linux/nsproxy.h>
 #include <linux/ptrace.h>
 #include <linux/freezer.h>
+#include <linux/hugetlb.h>
 
 #include <asm/futex.h>
 
@@ -241,7 +242,7 @@ again:
 	} else {
 		key->both.offset |= FUT_OFF_INODE; 
 		key->shared.inode = page_head->mapping->host;
-		key->shared.pgoff = page_head->index;
+		key->shared.pgoff = basepage_index(page);
 	}
 
 	get_futex_key_refs(key);
