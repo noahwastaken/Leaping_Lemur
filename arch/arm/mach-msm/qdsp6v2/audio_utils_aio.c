0be91718dce35dffb2051def1a688d58e7160341
Lens-F <tnoah12@gmail.com>
2014-05-08 16:58:22 -0400

    msm: audio: initialize the structure before sending to user space
    If uninitialzied structure is passed from kernel to user space, it
    may contain contain sensitive information that will be leaked.
    
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
+++ arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
@@ -1154,6 +1154,7 @@ long audio_aio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		struct msm_audio_stats stats;
 		uint64_t timestamp;
 		
+		memset(&stats, 0, sizeof(struct msm_audio_stats));
 		stats.byte_count = atomic_read(&audio->in_bytes);
 		stats.sample_count = atomic_read(&audio->in_samples);
 		q6asm_get_session_time(audio->ac, &timestamp);
