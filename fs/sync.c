298f80cf530e6e19a12ace69d87ecfb608a709f4
Lens-F <tnoah12@gmail.com>
2014-05-14 16:50:42 -0400

    squash Chad Goodman's dynamic fsync
    This reverts commit e7e8ec98157d480c78fe0924535bf63138d2ab32.
    
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -16,9 +16,6 @@
 #include "internal.h"
 
 #include <trace/events/mmcio.h>
-#ifdef CONFIG_DYNAMIC_FSYNC
-extern bool early_suspend_active;
-#endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -36,20 +33,20 @@ struct fsync_work {
 #endif
 static int __sync_filesystem(struct super_block *sb, int wait)
 {
-        if (sb->s_bdi == &noop_backing_dev_info)
-                return 0;
+	if (sb->s_bdi == &noop_backing_dev_info)
+		return 0;
 
-        if (sb->s_qcop && sb->s_qcop->quota_sync)
-                sb->s_qcop->quota_sync(sb, -1, wait);
+	if (sb->s_qcop && sb->s_qcop->quota_sync)
+		sb->s_qcop->quota_sync(sb, -1, wait);
 
-        if (wait)
-                sync_inodes_sb(sb);
-        else
-                writeback_inodes_sb(sb, WB_REASON_SYNC);
+	if (wait)
+		sync_inodes_sb(sb);
+	else
+		writeback_inodes_sb(sb, WB_REASON_SYNC);
 
-        if (sb->s_op->sync_fs)
-                sb->s_op->sync_fs(sb, wait);
-        return __sync_blockdev(sb->s_bdev, wait);
+	if (sb->s_op->sync_fs)
+		sb->s_op->sync_fs(sb, wait);
+	return __sync_blockdev(sb->s_bdev, wait);
 }
 
 int sync_filesystem(struct super_block *sb)
@@ -77,7 +74,7 @@ static void sync_one_sb(struct super_block *sb, void *arg)
 	if (!(sb->s_flags & MS_RDONLY))
 		__sync_filesystem(sb, *(int *)arg);
 }
-void sync_filesystems(int wait)
+static void sync_filesystems(int wait)
 {
 	iterate_supers(sync_one_sb, &wait);
 }
@@ -147,23 +144,12 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	int err;
-#ifdef CONFIG_DYNAMIC_FSYNC
-
-  if (!early_suspend_active)
-    return 0;
-  else {
-#endif
-
-
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
 	trace_vfs_fsync(file);
 	err = file->f_op->fsync(file, start, end, datasync);
 	trace_vfs_fsync_done(file);
 	return err;
-#ifdef CONFIG_DYNAMIC_FSYNC
-	 }
-#endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
@@ -309,21 +295,11 @@ no_async:
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
-#endif
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
-#endif
 	return do_fsync(fd, 1);
 }
 
@@ -339,11 +315,6 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else {
-#endif
 	int ret;
 	struct file *file;
 	struct address_space *mapping;
@@ -416,9 +387,6 @@ out_put:
 	fput_light(file, fput_needed);
 out:
 	return ret;
-#ifdef CONFIG_DYNAMIC_FSYNC
-  }
-#endif
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
 asmlinkage long SyS_sync_file_range(long fd, loff_t offset, loff_t nbytes,
@@ -433,11 +401,6 @@ SYSCALL_ALIAS(sys_sync_file_range, SyS_sync_file_range);
 SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
