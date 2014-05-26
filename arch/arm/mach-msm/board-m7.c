2876da91887bfdd9eff4019539556677eb166cfb
Lens-F <tnoah12@gmail.com>
2014-05-26 13:52:20 -0400

    board: tune msm_thermal values
    Lets cool down msm_thermal values...credit to @poondog for the tip
    
    Signed-off-by: Lens-F <tnoah12@gmail.com>

--- arch/arm/mach-msm/board-m7.c
+++ arch/arm/mach-msm/board-m7.c
@@ -3898,19 +3898,19 @@ static struct platform_device msm_tsens_device = {
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 400,
-	.shutdown_temp = 83,
+	.shutdown_temp = 79,
 
-	.allowed_max_high = 79,
-	.allowed_max_low = 74,
-	.allowed_max_freq = 300000,
+	.allowed_max_high = 75,
+	.allowed_max_low = 59,
+	.allowed_max_freq = 486000,
 
-	.allowed_mid_high = 76,
-	.allowed_mid_low = 71,
-	.allowed_mid_freq = 960000,
+	.allowed_mid_high = 60,
+	.allowed_mid_low = 47,
+	.allowed_mid_freq = 918000,
 
-	.allowed_low_high = 74,
-	.allowed_low_low = 68,
-	.allowed_low_freq = 1728000,
+	.allowed_low_high = 50,
+	.allowed_low_low = 40,
+	.allowed_low_freq = 1242000,
 };
 #endif
 
